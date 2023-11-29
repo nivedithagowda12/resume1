d5fefe0a8d51228ae8fd2890c49fc40887a9eb8a
nivedithagowda12 <152384594+nivedithagowda12@users.noreply.github.com>
2023-11-29 15:29:54 +0530

    Add files via upload
    blood

---
+++ script.js
@@ -0,0 +1,8 @@
+function bloodd() {
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
+if(age>=21){
+    alert(name+' ,your application submitted successfully🩸🩸')}
+    else{
+        alert(name+',you are not eligible🫢🫢')
+}}
